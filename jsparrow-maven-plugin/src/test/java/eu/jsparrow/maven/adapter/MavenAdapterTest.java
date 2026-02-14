c6b58a2605c0d1e24225a27ab78ece3af4541344
João Martins <jlabmartins@gmail.com>
2026-02-14 21:35:53 +0000

    Fix stubbing inconsistency
    Addresses https://github.com/Splendit/simonykees/pull/4#discussion_r2806742850

--- jsparrow-maven-plugin/src/test/java/eu/jsparrow/maven/adapter/MavenAdapterTest.java
+++ jsparrow-maven-plugin/src/test/java/eu/jsparrow/maven/adapter/MavenAdapterTest.java
@@ -115,7 +115,7 @@ public class MavenAdapterTest {
 		when(jsparrowTempDirectory.exists()).thenReturn(false);
 		when(jsparrowTempDirectory.mkdirs()).thenReturn(true);
 		when(jsparrowTempDirectory.getAbsolutePath()).thenReturn(absolutePath);
-		when(jsparrowTempDirectory.toPath()).thenReturn(directory.getRoot().toPath());
+		when(jsparrowTempDirectory.toPath()).thenReturn(Path.of(absolutePath));
 
 		mavenAdapter.prepareWorkingDirectory("");
 
